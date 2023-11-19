67ef6e97f7b8cf1e970d742d29177e755261db01
OminiCarlos <shuchen.mcgill@outlook.com>
2023-11-19 13:38:42 -0800

    fixed the timer

--- clock.js
+++ clock.js
@@ -1,42 +1,46 @@
 let state = 0;
-let timerId = null;  // Initialize timerId outside the Timer function
+let timerId = null;
+let timeLeft = 3; // Initialize timeLeft outside the Timer function
 
 document.getElementById('startButton').addEventListener('click', function() {
-    state = (state === 0) ? 1 : 0;
-    if (state === 1 && !timerId) {
-        Timer();
-    } else if (state === 0 && timerId) {
-        clearInterval(timerId);
-        timerId = null; // Clear the interval and reset the timerId when stopped
+    if (state === 0) {
+        if (!timerId) {
+            Timer(); // Start the timer only if it's not already running
+        }
+        state = 1;
+    } else {
+        clearInterval(timerId); // Pause the timer
+        timerId = null;
+        state = 0;
     }
 });
 
 document.getElementById('resetButton').addEventListener('click', function() {
     clearInterval(timerId);
-            timerId = null; // Reset the timerId when countdown finishes
-            alert("Reset!");
-            state = 0; // Reset state when countdown finishes
+    timerId = null;
+    alert("reset!");
+    state = 0; // Reset state when countdown finishes
+    timeLeft = 3; // Reset the timer for next start
 });
 
 function Timer() {
-    let timeLeft = 10; // Duration of the countdown in seconds
     let timerDisplay = document.getElementById('timerDisplay');
 
     timerId = setInterval(function() {
-        let minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
+        if (timeLeft > 0) {
+            let minutes = Math.floor(timeLeft / 60);
+            let seconds = timeLeft % 60;
 
-        // Format the time string
-        timerDisplay.textContent = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+            // Format the time string
+            timerDisplay.textContent = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 
-        if (timeLeft === 0) {
+            timeLeft--;
+        } else {
             clearInterval(timerId);
-            timerId = null; // Reset the timerId when countdown finishes
+            timerId = null;
             alert("Time's up!");
             state = 0; // Reset state when countdown finishes
+            timeLeft = 3; // Reset the timer for next start
         }
-
-        timeLeft--;
-
     }, 1000);
 }
