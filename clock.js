d330e2b8b400a87666ee4d2ec33f7e6e465be166
Julian Renzetti <julian.renzetti@outlook.com>
2023-11-19 10:25:37 -0800

    add base template for clock from chatgpt

--- clock.js
+++ clock.js
@@ -1,1 +1,19 @@
-// this is a tes
+document.getElementById('startButton').addEventListener('click', function() {
+    var timeLeft = 60; // Duration of the countdown in seconds
+    var timerDisplay = document.getElementById('timerDisplay');
+
+    var timerId = setInterval(function() {
+        var minutes = Math.floor(timeLeft / 60);
+        var seconds = timeLeft % 60;
+
+        // Format the time string
+        timerDisplay.textContent = minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+
+        if (timeLeft === 0) {
+            clearInterval(timerId);
+            alert("Time's up!");
+        }
+
+        timeLeft--;
+    }, 1000);
+});
