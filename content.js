8a50d9a28b8cb1fe3b69c5986a7959a0e11f39eb
Julian Renzetti <julian.renzetti@outlook.com>
2023-11-19 17:41:40 -0800

    blocks certain sites

---
+++ content.js
@@ -0,0 +1,18 @@
+switch (window.location.hostname){
+    case "www.youtube.com":
+         document.body.innerHTML = "<h1> BLOCKED </h1>";
+        break;
+    case "www.twitter.com":
+        document.body.innerHTML = "<h1> BLOCKED </h1>";
+        alert("twitter");
+        break;
+    case "www.facebook.com":
+        document.body.innerHTML = "<h1> BLOCKED </h1>";
+        break;
+    case "www.instagram.com":
+        document.body.innerHTML = "<h1> BLOCKED </h1>";
+        break;
+    case "www.pornhub.com":
+        document.body.innerHTML = "<h1> SHAME </h1>";
+        break;
+}
